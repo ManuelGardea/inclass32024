d75d79bf048fb091ec0a416272fd5bdc7ab6c6f7
ManuelGardea <mgardea021@gmail.com>
2024-04-02 18:32:09 -0400

    done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Counter from './components/Counter';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="counter-app">
+        <h1>Counter App</h1>
+        <Counter />
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+
+
